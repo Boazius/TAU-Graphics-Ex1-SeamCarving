9e9aa116c691ac3c25b1f7286ba006a1c37911fa
Boaz <Boazyakubov@gmail.com>
2022-03-19 19:31:37 +0200

    Added py files and exercise instructions.

---
+++ seam_carving.py
@@ -0,0 +1,20 @@
+from typing import Dict, Any
+
+NDArray = Any
+
+
+def resize(image: NDArray, out_height: int, out_width: int, forward_implementation: bool) -> Dict[str, NDArray]:
+    """
+
+    :param image: ِnp.array which represents an image.
+    :param out_height: the resized image height
+    :param out_width: the resized image width
+    :param forward_implementation: a boolean flag that indicates whether forward or basic implementation is used.
+                                    if forward_implementation is true then the forward-looking energy is used otherwise
+                                    the basic implementation is used.
+    :return: A dictionary with three elements, {'resized' : img1, 'vertical_seams' : img2 ,'horizontal_seams' : img3},
+            where img1 is the resized image and img2/img3 are the visualization images
+            (where the chosen seams are colored red and black for vertical and horizontal seams, respecitvely).
+    """
+    raise NotImplementedError('You need to implement this!')
+    # TODO: return { 'resized' : img1, 'vertical_seams' : img2 ,'horizontal_seams' : img3}
