f981ecee0459a02ce9b65385667af8be67a118b5
Boaz <Boazyakubov@gmail.com>
2022-04-05 02:44:20 +0300

    added some planned initializations for useful matrixes: backtracking, grayscale, matrices of original row and columns, and two output matrices for horizontal and vertical seams. will calculate cost matrix in the morning

--- seam_carving.py
+++ seam_carving.py
@@ -25,39 +25,62 @@ def resize(image: NDArray, out_height: int, out_width: int, forward_implementati
     widthDiff = out_width - width
     heightDiff = out_height - height
 
+    # initialize useful matrices - MAYBE MAKE THIS GLOBAL?
+
+    # TODO: saves the image converted to grayscale, WILL BE SHRANK AND ENLARGED
+    # grayscaleMat
+    # TODO: save for each cell its original row and column, WILL BE SHRANK AND ENLARGED
+    # originalColMat
+    # originalRowMat
+
+    # TODO: every cell will be 1 or 0, coloured in RED or not.
+    #  this will be in original height and width:
+    # outputVerticalSeamMat
+    # TODO: every cell will be 1 or 0, coloured in Black or not.
+    #  this will be in new shrunk/enlarged width (after removing/adding vertical seams),
+    #  but in original height (before adding/removing horizontal seams).
+    # outputHorizontalSeamMat
+
+    # TODO: cost matrix. WILL BE SHRANK AND ENLARGED. float value here.
+    # currCostMatrix
+    # TODO: backtracking matrix. for every cell, the value is either 1 2 or 3:
+    #  1 for upper left cell, 2 for upper cell, 3 for upper right cell
+    #  denotes the cell that gave the current cell its value in the cost matrix.
+    # currBackTrackingMatrix
+
     # add or remove k seams horizontally
     if heightDiff > 0:
-        # TODO add_k_seams instead
-        resized_image = remove_k_seams(image, out_height, out_width, forward_implementation, heightDiff)
+        resized_image = add_k_seams(image, out_height, out_width, forward_implementation, heightDiff)
     if heightDiff < 0:
         resized_image = remove_k_seams(image, out_height, out_width, forward_implementation, heightDiff)
 
     # rotate the image, add/remove k seams horizontally, and rotate back
     if widthDiff > 0:
         resized_image = rotate_image_counter_clockwise(
-            # TODO add_k_seams instead
-            remove_k_seams(rotate_image_clockwise(image, height, out_width),
-                           out_height, out_width, forward_implementation, widthDiff))
+            add_k_seams(rotate_image_clockwise(image, height, out_width),
+                        out_height, out_width, forward_implementation, widthDiff))
     if widthDiff < 0:
         resized_image = rotate_image_counter_clockwise(
             remove_k_seams(rotate_image_clockwise(image, height, out_width), out_height, out_width,
                            forward_implementation, widthDiff))
 
-    # To reduce image size by 𝑘 pixels, remove all chosen seams from the original image
-    # To enlarge by 𝑘 pixels, duplicate all chosen seams from the original image.
-
     # TODO: return { 'resized' : img1, 'vertical_seams' : img2 ,'horizontal_seams' : img3}
-
-    # TODO  when adding seams, we must find all k best seams using the same cost matrix, and only then
-    #  duplicate them all once.
-
-    # TODO, also keep for each pixel its original (in the original image) (i,j) index in a different matrix.
 
 
 def remove_k_seams(image: NDArray, out_height: int, out_width: int, forward_implementation: bool, k: int):
     # TODO this function removes the best seam from the image, k times.
     #  when deleting seams, we must delete one by one: i.e, calculate cost matrix, delete the seam, and calculate
     #  cost matrix again....
+    currHeight, currWidth = image.shape
+    for i in range(k):  # use range if you don't want to use tqdm
+        img = carve_column(image)
+    return img
+
+
+def add_k_seams(image: NDArray, out_height: int, out_width: int, forward_implementation: bool, k: int):
+    # TODO this function duplicates the best seam from the image, k times.
+    # TODO  when adding seams, we must find all k best seams using the same cost matrix, and only then
+    #   #  duplicate them all once.
     r, c = image.shape
     for i in range(k):  # use range if you don't want to use tqdm
         img = carve_column(image)
